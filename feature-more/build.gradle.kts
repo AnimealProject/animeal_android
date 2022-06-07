0755e70dfe52c97ad6189fce1750fd9201dc4d48
Fileek <balatayevvladislav@gmail.com>
2022-06-07 14:10:56 +0200

    Rebase onto develop branch

---
+++ feature-more/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = rootProject.extra["compileSdk"] as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra["minSdk"] as Int
+        targetSdk =  rootProject.extra["targetSdk"] as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["composeVersion"] as String
+    }
+}
+
+dependencies {
+    implementation(project(":base"))
+
+    val composeVersion: String by rootProject.extra
+    val activityComposeVersion: String by rootProject.extra
+
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.activity:activity-compose:$activityComposeVersion")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+}
