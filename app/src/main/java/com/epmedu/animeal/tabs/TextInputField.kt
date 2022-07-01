e55328306911219c053ffb8ebda517b5b53ddeb7
muhammadali_rasuljonov <muha.ras@mail.ru>
2022-07-01 20:16:26 +0500

    Detekt fixes

--- app/src/main/java/com/epmedu/animeal/tabs/TextInputField.kt
+++ app/src/main/java/com/epmedu/animeal/tabs/TextInputField.kt
@@ -1,21 +1,29 @@
 package com.epmedu.animeal.tabs
-
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.epmedu.animeal.base.theme.AnimealTheme
+import com.epmedu.animeal.base.theme.CursorColor
 import com.epmedu.animeal.base.theme.DarkerGrey
 import com.epmedu.animeal.base.theme.LynxWhite
 import com.epmedu.animeal.base.theme.NavigationItemColor
@@ -31,13 +39,11 @@ fun TextInputField(
 
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
 
         Text(
             text = title,
-            modifier = modifier.padding(bottom = 2.dp),
+            modifier = Modifier.padding(bottom = 2.dp),
             style = TextStyle(
                 color = NavigationItemColor,
                 fontSize = 14.sp
@@ -45,7 +51,7 @@ fun TextInputField(
         )
 
         TextField(
-            modifier = modifier.background(
+            modifier = Modifier.fillMaxWidth().background(
                 color = LynxWhite,
                 shape = RoundedCornerShape(12.dp),
             ),
@@ -57,7 +63,7 @@ fun TextInputField(
             colors = TextFieldDefaults.textFieldColors(
                 textColor = NavigationItemColor,
                 backgroundColor = Color.Transparent,
-                cursorColor = Color.Black,
+                cursorColor = CursorColor,
                 focusedIndicatorColor = Color.Transparent,
                 unfocusedIndicatorColor = Color.Transparent
             ),
@@ -68,22 +74,24 @@ fun TextInputField(
                     color = DarkerGrey
                 )
             },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            singleLine = true
         )
     }
 }
 
 @Preview(
     showBackground = true,
-    name = "TExtInputField",
+    name = "TextInputField",
     showSystemUi = true,
     uiMode = UI_MODE_NIGHT_NO,
 )
 @Composable
 fun TestPreviewInput() {
     AnimealTheme {
-        Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
+        Surface(
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
             var name by remember { mutableStateOf("") }
 
             TextInputField(
