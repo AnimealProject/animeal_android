6dc6801c6162b6d610490a52540931cbb43b170f
Fileek <balatayevvladislav@gmail.com>
2022-07-04 18:00:39 +0200

    Add more information about preview bug and add todo

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -12,7 +12,9 @@ dependencies {
 
     debugImplementation(libs.compose.ui.tooling)
 
-    // Temporary fix for preview bug https://issuetracker.google.com/issues/227767363
+    // TODO: Remove when fix come out
+    // Temporary fix for preview bug on Android Studio Chipmunk
+    // https://issuetracker.google.com/issues/227767363
     debugApi(libs.androidx.custom.view)
     debugApi(libs.androidx.custom.view.container)
 }
