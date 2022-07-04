32320823a2ff769ce010a723b9b50f7608f6c07f
Yury_Starzhynski <yura3882633@gmail.com>
2022-07-04 13:28:31 +0200

    EPMEDU-738 [Android] UI: Switcher between Dogs and Cats [Added example to HomeScreen]

--- app/src/main/java/com/epmedu/animeal/tabs/Screens.kt
+++ app/src/main/java/com/epmedu/animeal/tabs/Screens.kt
@@ -2,16 +2,24 @@ package com.epmedu.animeal.tabs
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.R
+import com.epmedu.animeal.base.ui.AnimealSwitch
 
 @Composable
 internal fun HomeScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
+        AnimealSwitch(
+            modifier = Modifier
+                .align(alignment = Alignment.TopCenter)
+                .padding(top = 24.dp),
+            onTabSelected = {})
         Text(text = stringResource(R.string.home), modifier = Modifier.align(Alignment.Center))
     }
 }
