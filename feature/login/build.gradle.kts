751342f2658003860e5e9d8299897ee451b6a276
Fileek <balatayevvladislav@gmail.com>
2022-07-06 11:54:00 +0200

    Fix issues after rebase

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
 
     implementation(libs.accompanist.pager)
     implementation(libs.accompanist.pager.indicators)
+
     implementation(libs.androidx.viewmodel)
     implementation(libs.androidx.viewmodel.compose)
 
--- library/foundation/src/main/java/com/epmedu/animeal/foundation/button/AnimealShortButton.kt
+++ library/foundation/src/main/java/com/epmedu/animeal/foundation/button/AnimealShortButton.kt
@@ -3,19 +3,15 @@ package com.epmedu.animeal.foundation.button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.base.theme.AnimealTheme
+import com.epmedu.animeal.base.theme.DisabledButtonColor
+import com.epmedu.animeal.base.theme.DisabledButtonContentColor
 
 @Composable
 fun AnimealShortButton(
@@ -30,8 +26,8 @@ fun AnimealShortButton(
         enabled = enabled,
         shape = RoundedCornerShape(30.dp),
         colors = ButtonDefaults.buttonColors(
-            disabledBackgroundColor = MaterialTheme.colors.secondaryVariant,
-            disabledContentColor = Color.White
+            disabledBackgroundColor = DisabledButtonColor,
+            disabledContentColor = DisabledButtonContentColor
         )
     ) {
         Text(
