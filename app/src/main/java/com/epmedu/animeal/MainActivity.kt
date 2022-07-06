3d6fefb9e102f9855c5649a271cb8e3446e26ee9
Fileek <balatayevvladislav@gmail.com>
2022-07-06 15:57:47 +0200

    Return keyboard padding

--- app/src/main/java/com/epmedu/animeal/MainActivity.kt
+++ app/src/main/java/com/epmedu/animeal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.view.WindowCompat
 import com.epmedu.animeal.base.theme.AnimealTheme
 import com.epmedu.animeal.common.screenRoute.MainScreenRoute
 import com.epmedu.animeal.login.OnboardingScreen
@@ -18,6 +19,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             AnimealTheme {
                 AnimatedScreenNavHost(
