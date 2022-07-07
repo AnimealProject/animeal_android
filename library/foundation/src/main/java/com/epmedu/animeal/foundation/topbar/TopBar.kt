7bc3477bd4b69f8480b8a2ea022c9b09948c5324
Fileek <balatayevvladislav@gmail.com>
2022-07-07 16:27:37 +0200

    Replaced Spacer with WidthSpacer

--- library/foundation/src/main/java/com/epmedu/animeal/foundation/topbar/TopBar.kt
+++ library/foundation/src/main/java/com/epmedu/animeal/foundation/topbar/TopBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.base.R
 import com.epmedu.animeal.base.theme.AnimealTheme
+import com.epmedu.animeal.foundation.spacer.WidthSpacer
 
 /**
  * Similar to [TopAppBar], but it consists only from a navigation icon and a title.
@@ -31,9 +32,9 @@ fun TopBar(
         verticalAlignment = Alignment.CenterVertically,
     ) {
         if (navigationIcon == null) {
-            Spacer(modifier = Modifier.width(44.dp))
+            WidthSpacer(44.dp)
         } else {
-            Spacer(modifier = Modifier.width(8.dp))
+            WidthSpacer(8.dp)
             navigationIcon()
         }
 
