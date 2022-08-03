9f0c0bf1b1afdd723785d870338044bbdedd7f6d
Yahor Urbanovich <egorikftp@gmail.com>
2022-08-03 13:14:34 +0300

    EPMDU-765: Fix detekt

--- app/src/main/java/com/epmedu/animeal/component/BuildConfigProviderImpl.kt
+++ app/src/main/java/com/epmedu/animeal/component/BuildConfigProviderImpl.kt
@@ -1,9 +1,9 @@
 package com.epmedu.animeal.component
 
-import com.epmedu.animeal.common.component.BuildConfigProvider
 import com.epmedu.animeal.BuildConfig
+import com.epmedu.animeal.common.component.BuildConfigProvider
 
-internal class BuildConfigProviderImpl: BuildConfigProvider {
+internal class BuildConfigProviderImpl : BuildConfigProvider {
 
     override val appVersion: String = BuildConfig.VERSION_NAME
 }
