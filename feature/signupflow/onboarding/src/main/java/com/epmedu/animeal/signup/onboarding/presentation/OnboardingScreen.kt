62f3e3921334dd7cf8f40e0c014981aaa91e1773
Mikita <17089577+n3gbx@users.noreply.github.com>
2022-08-23 08:27:13 +0200

    EPMEDU-819 Fix Facebook login flow to match mobile one (#53)
    * Fix Sign in with facebook flow
    
    This reverts commit 7cb2efe9cf85eadf1b9e90e6113be9817d2e4932.

--- feature/signupflow/onboarding/src/main/java/com/epmedu/animeal/signup/onboarding/presentation/OnboardingScreen.kt
+++ feature/signupflow/onboarding/src/main/java/com/epmedu/animeal/signup/onboarding/presentation/OnboardingScreen.kt
@@ -3,8 +3,6 @@ package com.epmedu.animeal.signup.onboarding.presentation
 import androidx.compose.runtime.Composable
 import com.epmedu.animeal.extensions.currentOrThrow
 import com.epmedu.animeal.navigation.navigator.LocalNavigator
-import com.epmedu.animeal.navigation.navigator.Navigator
-import com.epmedu.animeal.navigation.route.MainRoute
 import com.epmedu.animeal.navigation.route.SignUpRoute
 
 @Composable
@@ -16,16 +14,7 @@ fun OnboardingScreen() {
             navigator.navigate(SignUpRoute.EnterPhone.name)
         },
         onSignInFacebook = {
-            navigator.navigateToTabs()
+            navigator.navigate(SignUpRoute.EnterPhone.name)
         },
     )
-}
-
-// TODO: Remove when facebook login will be implemented
-private fun Navigator.navigateToTabs() {
-    parent?.navigate(MainRoute.Tabs.name) {
-        popUpTo(MainRoute.SignUp.name) {
-            inclusive = true
-        }
-    }
 }
