d5cc50c599dccc33517876911b9e5409141fff6e
Mikita <17089577+n3gbx@users.noreply.github.com>
2022-09-12 17:40:32 +0200

    Add feed status icons, strings and model

---
+++ feature/home/src/main/java/com/epmedu/animeal/home/data/model/FeedStatus.kt
@@ -0,0 +1,17 @@
+package com.epmedu.animeal.home.data.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
+import com.epmedu.animeal.foundation.theme.CustomColor
+import com.epmedu.animeal.resources.R
+
+enum class FeedStatus(
+    @StringRes val titleId: Int,
+    @DrawableRes val iconId: Int,
+    val color: Color
+) {
+    GREEN(R.string.feed_status_green, R.drawable.ic_face_happy, CustomColor.StatusGreen),
+    ORANGE(R.string.feed_status_orange, R.drawable.ic_face_neutral, CustomColor.StatusOrange),
+    RED(R.string.feed_status_red, R.drawable.ic_face_upset, CustomColor.StatusRed);
+}
