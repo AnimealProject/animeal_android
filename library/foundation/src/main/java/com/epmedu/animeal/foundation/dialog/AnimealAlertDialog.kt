bc3ef52a2d45fb9e76a846d247a373f9d655ad0d
Khvostov Maksim <mkliverout@gmail.com>
2022-09-26 13:43:37 +0300

    [EPMEDU-889]: Fix cancel button in AlertDialog
    - Replace AnimealSecondaryButton to AnimealSecondaryButtonOutlined

--- library/foundation/src/main/java/com/epmedu/animeal/foundation/dialog/AnimealAlertDialog.kt
+++ library/foundation/src/main/java/com/epmedu/animeal/foundation/dialog/AnimealAlertDialog.kt
@@ -1,6 +1,9 @@
 package com.epmedu.animeal.foundation.dialog
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.MaterialTheme
@@ -11,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.foundation.button.AnimealButton
-import com.epmedu.animeal.foundation.button.AnimealSecondaryButton
+import com.epmedu.animeal.foundation.button.AnimealSecondaryButtonOutlined
 import com.epmedu.animeal.foundation.preview.AnimealPreview
 import com.epmedu.animeal.foundation.theme.AnimealTheme
 
@@ -46,7 +49,7 @@ fun AnimealAlertDialog(
                 horizontalArrangement = Arrangement.spacedBy(12.dp),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                AnimealSecondaryButton(
+                AnimealSecondaryButtonOutlined(
                     modifier = Modifier.weight(1f),
                     text = dismissText,
                     onClick = {
