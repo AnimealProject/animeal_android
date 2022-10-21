b1863747c21b22d64840d895bb217af66dde5ddb
Andrei Klepets <ak1988.88@gmail.com>
2022-10-21 18:49:47 -0400

    [EPMEDU-953] Create Thank you page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ android {
 }
 
 dependencies {
+    implementation(projects.feature.home)
+
     implementation(projects.feature.signupflow.signup)
     implementation(projects.feature.splash)
     implementation(projects.feature.tabsflow.tabs)
