d6b52efd5d3255eb5a0e388bb2f582bf00740078
Andrei Klepets <ak1988.88@gmail.com>
2022-10-27 11:13:48 -0400

    [EPMEDU-953] Create Thank you page (#97)
    * [EPMEDU-953] Create Thank you page

---
+++ feature/home/src/main/java/com/epmedu/animeal/home/presentation/thankyou/ThankYouScreenUi.kt
@@ -0,0 +1,44 @@
+package com.epmedu.animeal.home.presentation.thankyou
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.epmedu.animeal.foundation.button.AnimealButton
+import com.epmedu.animeal.foundation.preview.AnimealPreview
+import com.epmedu.animeal.foundation.theme.AnimealTheme
+import com.epmedu.animeal.home.presentation.thankyou.ui.ThankYouContent
+import com.epmedu.animeal.resources.R
+
+@Composable
+internal fun ThankYouScreenUI(
+    modifier: Modifier = Modifier,
+    onBackToHome: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        ThankYouContent()
+        Spacer(modifier = Modifier.weight(1f))
+        AnimealButton(
+            modifier = Modifier.padding(30.dp),
+            text = stringResource(R.string.back_to_home),
+            onClick = onBackToHome
+        )
+    }
+}
+
+@AnimealPreview
+@Composable
+private fun ThankYouScreenPreview() {
+    AnimealTheme {
+        ThankYouScreenUI(
+            onBackToHome = {},
+        )
+    }
+}
