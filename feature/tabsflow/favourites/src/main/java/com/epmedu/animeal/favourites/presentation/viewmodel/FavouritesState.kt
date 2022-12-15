af8765a6852518912a00a2702e455e252e1197d0
Andrii Chornyi <andrii_chornyi@epam.com>
2022-12-15 19:54:25 +0200

    [EPMEDU-1078]: updated FavouritesState after it was moved with replacing kotlin list on immutable list

--- feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/presentation/viewmodel/FavouritesState.kt
+++ feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/presentation/viewmodel/FavouritesState.kt
@@ -1,9 +1,11 @@
 package com.epmedu.animeal.favourites.presentation.viewmodel
 
 import com.epmedu.animeal.feeding.domain.model.FeedingPoint
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 data class FavouritesState(
-    val favourites: List<FeedingPoint> = emptyList(),
+    val favourites: ImmutableList<FeedingPoint> = persistentListOf(),
     val showingFeedSpot: FeedingPoint? = null,
     val showingWillFeedDialog: Boolean = false,
 )
