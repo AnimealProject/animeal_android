ffa88235612eedebf6e1c82df1a34c79062afb8e
ArtemKleinschmidt <artem.kleinschmidt@gmail.com>
2022-12-23 16:54:12 +0200

    Convert a picture to webp Revert OnboardingScreen.kt

--- feature/signupflow/onboarding/src/main/java/com/epmedu/animeal/signup/onboarding/presentation/OnboardingScreen.kt
+++ feature/signupflow/onboarding/src/main/java/com/epmedu/animeal/signup/onboarding/presentation/OnboardingScreen.kt
@@ -1,11 +1,9 @@
 package com.epmedu.animeal.signup.onboarding.presentation
 
 import androidx.compose.runtime.Composable
-import com.epmedu.animeal.common.route.MainRoute
 import com.epmedu.animeal.common.route.SignUpRoute
 import com.epmedu.animeal.extensions.currentOrThrow
 import com.epmedu.animeal.navigation.navigator.LocalNavigator
-import com.epmedu.animeal.navigation.navigator.Navigator
 
 @Composable
 fun OnboardingScreen() {
@@ -16,15 +14,7 @@ fun OnboardingScreen() {
             navigator.navigate(SignUpRoute.EnterPhone.name)
         },
         onSignInFacebook = {
-            navigator.navigateToTabs()
+            navigator.navigate(SignUpRoute.EnterPhone.name)
         },
     )
-}
-
-private fun Navigator.navigateToTabs() {
-    parent?.navigate(MainRoute.Tabs.name) {
-        popUpTo(MainRoute.SignUp.name) {
-            inclusive = true
-        }
-    }
 }
