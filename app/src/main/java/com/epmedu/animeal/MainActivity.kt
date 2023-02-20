862cc51a3590b0a36e516ecf3c81c0741885c7b7
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2023-02-20 19:38:00 +0100

    Pass onError in Amplify

--- app/src/main/java/com/epmedu/animeal/MainActivity.kt
+++ app/src/main/java/com/epmedu/animeal/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity :
             { signInResult ->
                 if (signInResult.isSignInComplete) onSuccess()
             },
-            {}
+            onError
         )
     }
 }
