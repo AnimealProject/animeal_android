9ab9567e9187d5a60ed27da11bf0028afdaf4d69
Andrii-Chornyi <112079677+Andrii-Chornyi@users.noreply.github.com>
2023-02-22 16:43:32 +0200

    [EPMEDU-1270]: Add html markup support for feeding points description (#122)
    * [EPMEDU-1270]: Add html markup support for feeding points description

--- library/foundation/build.gradle.kts
+++ library/foundation/build.gradle.kts
@@ -21,5 +21,7 @@ dependencies {
     implementation(libs.compose.material.dialog.core)
     implementation(libs.compose.material.dialog.datetime)
 
+    implementation(libs.htmlText)
+
     debugImplementation(libs.compose.ui.tooling)
 }
