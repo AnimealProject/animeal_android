3df3ca5ba0dcd36e63b22de7acc9f241617c88c1
Vladislav Balatayev <83027107+Fileek@users.noreply.github.com>
2023-02-24 15:35:15 +0100

    [EPMEDU-1526]: Newly registered User is not saved in Animeal Studio (#134)
    * [EPMEDU-1526]: Newly registered User is not saved in Animeal Studio
    
      - Fix profile mapping
    
    * Create UserAttributesKey object to hold keys
    
    ---------
    
    Co-authored-by: Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>

--- feature/signupflow/finishprofile/src/main/java/com/epmedu/animeal/signup/finishprofile/presentation/viewmodel/FinishProfileViewModel.kt
+++ feature/signupflow/finishprofile/src/main/java/com/epmedu/animeal/signup/finishprofile/presentation/viewmodel/FinishProfileViewModel.kt
@@ -27,6 +27,7 @@ import com.epmedu.animeal.signup.finishprofile.presentation.viewmodel.FinishProf
 import com.epmedu.animeal.signup.finishprofile.presentation.viewmodel.FinishProfileEvent.ProfileFinished
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @Suppress("LongParameterList")
@@ -158,13 +159,18 @@ internal class FinishProfileViewModel @Inject constructor(
             saveProfileUseCase(state.profile).collect {
                 updateNetworkProfileUseCase.invoke(
                     profile = state.profile,
-                    onSuccess = {},
+                    onSuccess = {
+                        runBlocking {
+                            sendEvent(
+                                when (authenticationType) {
+                                    AuthenticationType.Mobile -> ProfileFinished
+                                    is AuthenticationType.Facebook -> NavigateToConfirmPhone
+                                }
+                            )
+                        }
+                    },
                     onError = {}
                 )
-                when (authenticationType) {
-                    AuthenticationType.Mobile -> sendEvent(ProfileFinished)
-                    is AuthenticationType.Facebook -> sendEvent(NavigateToConfirmPhone)
-                }
             }
         }
     }
