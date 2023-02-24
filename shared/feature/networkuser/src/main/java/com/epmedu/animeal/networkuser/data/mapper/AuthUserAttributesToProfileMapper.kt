7a63b429fd0c41ba5fc404a0cce347f29501ac0e
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2023-02-24 15:02:15 +0100

    [EPMEDU-1526]: Newly registered User is not saved in Animeal Studio
      - Fix profile mapping

--- shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/mapper/AuthUserAttributesToProfileMapper.kt
+++ shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/mapper/AuthUserAttributesToProfileMapper.kt
@@ -3,7 +3,7 @@ package com.epmedu.animeal.networkuser.data.mapper
 import com.amplifyframework.auth.AuthUserAttribute
 import com.amplifyframework.auth.AuthUserAttributeKey
 import com.epmedu.animeal.extensions.DAY_MONTH_NAME_COMMA_YEAR_FORMATTER
-import com.epmedu.animeal.extensions.MONTH_DAY_YEAR_SLASH_FORMATTER
+import com.epmedu.animeal.extensions.DAY_MONTH_YEAR_SLASH_FORMATTER
 import com.epmedu.animeal.extensions.reformatDateToString
 import com.epmedu.animeal.foundation.common.validation.Constants.GE_PHONE_NUMBER_LENGTH
 import com.epmedu.animeal.profile.data.model.Profile
@@ -15,12 +15,12 @@ class AuthUserAttributesToProfileMapper {
         val convertedLocalFormatDate = birthDate?.let {
             reformatDateToString(
                 dateString = birthDate,
-                originalFormatter = MONTH_DAY_YEAR_SLASH_FORMATTER,
+                originalFormatter = DAY_MONTH_YEAR_SLASH_FORMATTER,
                 newFormatter = DAY_MONTH_NAME_COMMA_YEAR_FORMATTER
             )
         } ?: ""
         return Profile(
-            name = attributes.find { it.key == AuthUserAttributeKey.givenName() }?.value ?: "",
+            name = attributes.find { it.key == AuthUserAttributeKey.name() }?.value ?: "",
             surname = attributes.find { it.key == AuthUserAttributeKey.familyName() }?.value ?: "",
             birthDate = convertedLocalFormatDate,
             phoneNumber = attributes.find { it.key == AuthUserAttributeKey.phoneNumber() }?.value
--- shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/mapper/ProfileToAuthUserAttributesMapper.kt
+++ shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/mapper/ProfileToAuthUserAttributesMapper.kt
@@ -3,10 +3,9 @@ package com.epmedu.animeal.networkuser.data.mapper
 import com.amplifyframework.auth.AuthUserAttribute
 import com.amplifyframework.auth.AuthUserAttributeKey
 import com.epmedu.animeal.extensions.DAY_MONTH_NAME_COMMA_YEAR_FORMATTER
-import com.epmedu.animeal.extensions.MONTH_DAY_YEAR_SLASH_FORMATTER
+import com.epmedu.animeal.extensions.DAY_MONTH_YEAR_SLASH_FORMATTER
 import com.epmedu.animeal.extensions.reformatDateToString
 import com.epmedu.animeal.profile.data.model.Profile
-import com.epmedu.animeal.profile.domain.model.getFormat
 
 class ProfileToAuthUserAttributesMapper {
 
@@ -14,16 +13,16 @@ class ProfileToAuthUserAttributesMapper {
         val convertedAmplifyFormatDate = reformatDateToString(
             dateString = profile.birthDate,
             originalFormatter = DAY_MONTH_NAME_COMMA_YEAR_FORMATTER,
-            newFormatter = MONTH_DAY_YEAR_SLASH_FORMATTER
+            newFormatter = DAY_MONTH_YEAR_SLASH_FORMATTER
         )
         return listOf(
-            AuthUserAttribute(AuthUserAttributeKey.givenName(), profile.name),
+            AuthUserAttribute(AuthUserAttributeKey.name(), profile.name),
             AuthUserAttribute(AuthUserAttributeKey.familyName(), profile.surname),
             AuthUserAttribute(AuthUserAttributeKey.email(), profile.email),
             AuthUserAttribute(AuthUserAttributeKey.birthdate(), convertedAmplifyFormatDate),
             AuthUserAttribute(
                 AuthUserAttributeKey.phoneNumber(),
-                profile.phoneNumberRegion.getFormat() + profile.phoneNumber
+                profile.phoneNumberRegion.phoneNumberCode + profile.phoneNumber
             )
         )
     }
