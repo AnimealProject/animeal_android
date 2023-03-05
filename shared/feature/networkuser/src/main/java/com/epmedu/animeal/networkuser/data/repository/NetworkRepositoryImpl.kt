dbbfec89ab4123525046ead0500f5aeaf88439df
Andrii-Chornyi <112079677+Andrii-Chornyi@users.noreply.github.com>
2023-03-06 00:10:59 +0200

    [EPMEDU-1527] - Clear local profile after leaving Facebook flow (#138)
    * [EPMEDU-1527] Clear local profile after leaving facebook flow
    
    * [EPMEDU-1527] removed usage of deprecated AuthRequestHandler from NetworkApi
    
    * [EPMEDU-1527] updated NetworkRepository to use ActionResult and ActionDelegate
    
    * [EPMEDU-1527] clearing profile on account page logout
    
    * [EPMEDU-1527] fixed issue with not moving to the next screen for facebook flow on FinishProfile screen

--- shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/repository/NetworkRepositoryImpl.kt
+++ shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/repository/NetworkRepositoryImpl.kt
@@ -3,10 +3,10 @@ package com.epmedu.animeal.networkuser.data.repository
 import com.amplifyframework.auth.AuthUserAttribute
 import com.amplifyframework.auth.AuthUserAttributeKey
 import com.epmedu.animeal.auth.AuthAPI
-import com.epmedu.animeal.auth.AuthRequestHandler
 import com.epmedu.animeal.auth.UserAttributesAPI
 import com.epmedu.animeal.auth.constants.UserAttributesKey
 import com.epmedu.animeal.common.data.wrapper.ApiResult
+import com.epmedu.animeal.common.domain.wrapper.ActionResult
 import com.epmedu.animeal.networkuser.data.mapper.AuthUserAttributesToProfileMapper
 import com.epmedu.animeal.networkuser.data.mapper.ProfileToAuthUserAttributesMapper
 import com.epmedu.animeal.networkuser.domain.repository.NetworkRepository
@@ -39,11 +39,18 @@ class NetworkRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun updateNetworkUserAttributes(profile: Profile, authRequestHandler: AuthRequestHandler) {
-        userAttributesAPI.updateUserAttributes(profileToAuthUserMapper.map(profile), authRequestHandler)
+    override suspend fun updateNetworkUserAttributes(profile: Profile): ActionResult {
+        return userAttributesAPI.updateUserAttributes(profileToAuthUserMapper.map(profile)).toActionResult()
     }
 
-    override suspend fun deleteNetworkUser(authRequestHandler: AuthRequestHandler) {
-        userAttributesAPI.deleteUser(authRequestHandler)
+    override suspend fun deleteNetworkUser(): ActionResult {
+        return userAttributesAPI.deleteUser().toActionResult()
+    }
+}
+
+private fun ApiResult<Unit>.toActionResult(): ActionResult {
+    return when (this) {
+        is ApiResult.Success -> ActionResult.Success
+        is ApiResult.Failure -> ActionResult.Failure(error)
     }
 }
---
+++ shared/feature/profile/src/main/java/com/epmedu/animeal/profile/domain/ClearProfileUseCase.kt
@@ -0,0 +1,16 @@
+package com.epmedu.animeal.profile.domain
+
+import com.epmedu.animeal.profile.data.repository.ProfileRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ClearProfileUseCase(private val repository: ProfileRepository) {
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    operator fun invoke() {
+        coroutineScope.launch {
+            repository.clearProfile()
+        }
+    }
+}
