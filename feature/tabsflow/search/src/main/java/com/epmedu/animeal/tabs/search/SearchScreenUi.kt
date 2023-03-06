f50f718c18a2d6a293e35dc9e2db7fa288bd1657
VEZE <yura3882633@gamil.com>
2023-03-06 20:35:51 +0100

    Fixed AnimalTypeHorizontalPager content padding, moved to SearchScreenUI

--- feature/tabsflow/search/src/main/java/com/epmedu/animeal/tabs/search/SearchScreenUi.kt
+++ feature/tabsflow/search/src/main/java/com/epmedu/animeal/tabs/search/SearchScreenUi.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.foundation.preview.AnimealPreview
-import com.epmedu.animeal.foundation.switch.AnimalTypeHorizontalPager
+import com.epmedu.animeal.foundation.switch.AnimealPagerTabRow
 import com.epmedu.animeal.foundation.switch.model.AnimalType
 import com.epmedu.animeal.foundation.theme.AnimealTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun SearchScreenUi() {
@@ -41,6 +47,35 @@ internal fun SearchScreenUi() {
                 AnimalType.Cats -> Text("Cats")
             }
         }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun AnimalTypeHorizontalPager(
+    modifier: Modifier = Modifier,
+    content: @Composable (animalType: AnimalType) -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+
+    val pages = remember { AnimalType.values() }
+    val pagerState = rememberPagerState()
+
+    Column() {
+        AnimealPagerTabRow(
+            modifier = modifier,
+            pagerState = pagerState,
+            onSelectTab = { tabIndex ->
+                scope.launch {
+                    pagerState.animateScrollToPage(tabIndex)
+                }
+            })
+
+        HorizontalPager(
+            count = pages.size,
+            state = pagerState,
+            content = { page -> content(pages[page]) }
+        )
     }
 }
 
