cd1dafde7dd9fbbedb56f26fb2338d694fcdf3e9
Vladislav Balatayev <83027107+Fileek@users.noreply.github.com>
2023-03-14 12:12:05 +0100

    Fix fetching and saving phone number prefix and region (#147)
    Co-authored-by: Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>

--- shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/mapper/AuthUserAttributesToProfileMapper.kt
+++ shared/feature/networkuser/src/main/java/com/epmedu/animeal/networkuser/data/mapper/AuthUserAttributesToProfileMapper.kt
@@ -7,12 +7,13 @@ import com.epmedu.animeal.auth.constants.UserAttributesKey.emailKey
 import com.epmedu.animeal.auth.constants.UserAttributesKey.nameKey
 import com.epmedu.animeal.auth.constants.UserAttributesKey.phoneNumberKey
 import com.epmedu.animeal.auth.constants.UserAttributesKey.surnameKey
+import com.epmedu.animeal.common.constants.DefaultConstants.EMPTY_STRING
 import com.epmedu.animeal.extensions.DAY_MONTH_NAME_COMMA_YEAR_FORMATTER
 import com.epmedu.animeal.extensions.DAY_MONTH_YEAR_SLASH_FORMATTER
 import com.epmedu.animeal.extensions.MONTH_DAY_YEAR_SLASH_FORMATTER
 import com.epmedu.animeal.extensions.reformatDateToString
-import com.epmedu.animeal.foundation.common.validation.Constants.GE_PHONE_NUMBER_LENGTH
 import com.epmedu.animeal.profile.data.model.Profile
+import com.epmedu.animeal.profile.domain.model.Region
 
 class AuthUserAttributesToProfileMapper {
 
@@ -31,13 +32,19 @@ class AuthUserAttributesToProfileMapper {
                 newFormatter = DAY_MONTH_NAME_COMMA_YEAR_FORMATTER
             )
         } ?: ""
+        val phoneNumberWithPrefix = attributes.find { it.key == phoneNumberKey }?.value
+        val phoneNumberRegion = phoneNumberWithPrefix?.let {
+            Region.values().find { phoneNumberWithPrefix.startsWith(it.phoneNumberCode) }
+        } ?: Region.GE
+        val phoneNumber =
+            phoneNumberWithPrefix?.removePrefix(phoneNumberRegion.phoneNumberCode) ?: EMPTY_STRING
         return Profile(
             name = attributes.find { it.key == nameKey }?.value ?: "",
             surname = attributes.find { it.key == surnameKey }?.value ?: "",
             birthDate = convertedLocalFormatDate,
-            phoneNumber = attributes.find { it.key == phoneNumberKey }?.value
-                ?.takeLast(GE_PHONE_NUMBER_LENGTH) ?: "",
-            email = attributes.find { it.key == emailKey }?.value ?: ""
+            phoneNumberRegion = phoneNumberRegion,
+            phoneNumber = phoneNumber,
+            email = attributes.find { it.key == emailKey }?.value ?: "",
         )
     }
 }
