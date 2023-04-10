ca2c49149a8024fb0fc64f82178fd6dc62b01a75
ozkarram <ozkar.ram.dev@gmail.com>
2023-04-10 22:20:30 +0300

    Adds missing dependencies to about's gradle component

--- feature/tabsflow/moreflow/about/build.gradle.kts
+++ feature/tabsflow/moreflow/about/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("AnimealPlugin")
     id("com.android.library")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -10,6 +12,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
 
+    implementation(projects.library.common)
     implementation(projects.library.extensions)
     implementation(projects.library.foundation)
     implementation(projects.library.navigation)
@@ -19,4 +22,7 @@ dependencies {
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.preview)
     debugImplementation(libs.compose.ui.tooling)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 }
