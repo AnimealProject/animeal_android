98b40111d45b5d67a6597044d135b29ed252dee8
ozkarram <ozkar.ram.dev@gmail.com>
2023-04-18 22:01:53 +0300

    Fix rebase issue

--- shared/feature/feedconfirmation/src/main/java/com/epmedu/animeal/feedconfirmation/domain/StartFeedingPointUseCase.kt
+++ shared/feature/feedconfirmation/src/main/java/com/epmedu/animeal/feedconfirmation/domain/StartFeedingPointUseCase.kt
@@ -1,8 +1,8 @@
 package com.epmedu.animeal.feedconfirmation.domain
 
-import com.epmedu.animeal.feeding.domain.repository.FeedingPointRepository
+import com.epmedu.animeal.feeding.domain.repository.FeedingRepository
 import javax.inject.Inject
 
-class StartFeedingPointUseCase @Inject constructor(private val feedingPointRepository: FeedingPointRepository) {
-    suspend operator fun invoke(feedingPointId: String) = feedingPointRepository.startFeeding(feedingPointId)
+class StartFeedingPointUseCase @Inject constructor(private val feedingRepository: FeedingRepository) {
+    suspend operator fun invoke(feedingPointId: String) = feedingRepository.startFeeding(feedingPointId)
 }
