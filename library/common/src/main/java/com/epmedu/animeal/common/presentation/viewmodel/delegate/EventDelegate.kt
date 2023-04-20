53bb62ccc7e6b94ce2441f3cb05d95759a4c68c3
Vladislav Balatayev <balatayevvladislav@gmail.com>
2023-04-20 17:51:57 +0200

    Deprecate EventDelegate

--- library/common/src/main/java/com/epmedu/animeal/common/presentation/viewmodel/delegate/EventDelegate.kt
+++ library/common/src/main/java/com/epmedu/animeal/common/presentation/viewmodel/delegate/EventDelegate.kt
@@ -2,6 +2,14 @@ package com.epmedu.animeal.common.presentation.viewmodel.delegate
 
 import kotlinx.coroutines.flow.SharedFlow
 
+@Deprecated(
+    message = """ViewModel events is an antipattern. 
+            Usages of this interface should be replaced with StateDelegate or moved to UI if there is no business logic.
+            Please read the following article to learn more: 
+            https://medium.com/androiddevelopers/viewmodel-one-off-event-antipatterns-16a1da869b95""",
+    replaceWith = ReplaceWith("StateDelegate"),
+    level = DeprecationLevel.WARNING
+)
 interface EventDelegate<Event> {
 
     val events: SharedFlow<Event>
