74a4ce2bbe967fb8aaf4e485e97421ed5adafc03
ozkarram <ozkar.ram.dev@gmail.com>
2023-04-21 04:18:50 -0700

    [EPMEDU-1924] Create an image loader and uploader (#177)
    * Files uploading logic
    
    * Adds parseAmplifyUrl to StorageApi
    
    * Fix detekt issues
    
    ---------
    
    Co-authored-by: Andrii Chornyi <andrii_chornyi@epam.com>

---
+++ library/api/src/main/java/com/epmedu/animeal/api/storage/StorageApiImpl.kt
@@ -0,0 +1,36 @@
+package com.epmedu.animeal.api.storage
+
+import android.net.Uri
+import androidx.core.net.toFile
+import com.amplifyframework.core.Amplify
+import com.amplifyframework.storage.options.StorageGetUrlOptions
+import com.epmedu.animeal.common.data.wrapper.ApiResult
+import com.epmedu.animeal.extensions.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+internal class StorageApiImpl : StorageApi {
+
+    override suspend fun uploadFile(fileName: String, uri: Uri): ApiResult<Unit> {
+        return suspendCancellableCoroutine {
+            val file = uri.toFile()
+
+            Amplify.Storage.uploadFile(
+                fileName,
+                file,
+                { resume(ApiResult.Success(Unit)) },
+                { resume(ApiResult.Failure(it)) }
+            )
+        }
+    }
+
+    override suspend fun parseAmplifyUrl(imageId: String): ApiResult<String> =
+        suspendCancellableCoroutine {
+            val options = StorageGetUrlOptions.builder().build()
+            Amplify.Storage.getUrl(
+                imageId,
+                options,
+                { resume(ApiResult.Success(it.url.toString())) },
+                { resume(ApiResult.Failure(it)) }
+            )
+        }
+}
