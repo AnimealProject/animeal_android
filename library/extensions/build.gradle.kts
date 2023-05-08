c37c71c0f744fbc0a97c89b61034a49cd417ba1c
yuriiq <8038930+yuriiq@users.noreply.github.com>
2023-05-08 21:49:21 +0200

    EPMEDU-1079 added extension

--- library/extensions/build.gradle.kts
+++ library/extensions/build.gradle.kts
@@ -8,14 +8,11 @@ android {
 }
 
 dependencies {
+    api(libs.androidx.datastore)
     implementation(projects.library.common)
     implementation(projects.library.resources)
-
     implementation(platform(libs.androidx.compose.bom))
-
     implementation(libs.androidx.appcompat)
     implementation(libs.compose.runtime)
-
     implementation(libs.play.services.location)
-
 }
