0c98bc520344df9cab283da6f8625f6a16330bab
yuriiq <8038930+yuriiq@users.noreply.github.com>
2023-05-11 13:49:12 +0200

    [refactor/EPMEDU-1079]: Improve data store interactions (#192)
    * added Datastore edit() extension
    
    * EPMEDU-1079 replace DataStorePreferencesKey
    
    * EPMEDU-1079  split DataStorePreferencesKey
    
    * EPMEDU-1079 update DataStorePreferencesKey usage

---
+++ app/src/main/java/com/epmedu/animeal/component/DataStorePreferences.kt
@@ -0,0 +1,28 @@
+package com.epmedu.animeal.component
+
+import androidx.datastore.preferences.core.MutablePreferences
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.epmedu.animeal.common.constants.DefaultConstants
+
+private val ANIMAL_TYPE = stringPreferencesKey("AnimalType")
+val Preferences.animalType: String
+    get() = this[ANIMAL_TYPE] ?: DefaultConstants.EMPTY_STRING
+fun MutablePreferences.updateAnimalType(value: String) {
+    this[ANIMAL_TYPE] = value
+}
+
+private val INITIAL_GEOLOCATION_PERMISSION = booleanPreferencesKey("InitialGeolocationPermissionOnHomeScreen")
+val Preferences.initialGeolocationPermission: Boolean
+    get() = this[INITIAL_GEOLOCATION_PERMISSION] ?: false
+fun MutablePreferences.updateInitialGeolocationPermission(value: Boolean) {
+    this[INITIAL_GEOLOCATION_PERMISSION] = value
+}
+
+private val INITIAL_CAMERA_PERMISSION = booleanPreferencesKey("InitialCameraPermissionOnHomeScreen")
+val Preferences.initialCameraPermission: Boolean
+    get() = this[INITIAL_CAMERA_PERMISSION] ?: false
+fun MutablePreferences.updateInitialCameraPermission(value: Boolean) {
+    this[INITIAL_CAMERA_PERMISSION] = value
+}
