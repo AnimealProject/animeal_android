aab7b46fd5f3785bb16699848f472ee2ae317560
Vladislav Balatayev <balatayevvladislav@gmail.com>
2023-05-20 00:02:33 +0200

    [EPMEDU-968]: Open the feeding point details from the Favorites page
      - Add feedings to Favourites page

--- feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/presentation/FavouritesScreenEvent.kt
+++ feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/presentation/FavouritesScreenEvent.kt
@@ -1,13 +1,13 @@
 package com.epmedu.animeal.favourites.presentation
 
-import com.epmedu.animeal.feeding.domain.model.FeedingPoint
+import com.epmedu.animeal.feeding.presentation.model.FeedingPointModel
 
 internal sealed interface FavouritesScreenEvent {
-    data class FeedingPointSelected(val feedingPoint: FeedingPoint) : FavouritesScreenEvent
+    data class FeedingPointSelected(val feedingPoint: FeedingPointModel) : FavouritesScreenEvent
     object FeedingPointHidden : FavouritesScreenEvent
 
     data class FavouriteChange(
         val isFavourite: Boolean,
-        val feedingPoint: FeedingPoint
+        val feedingPoint: FeedingPointModel
     ) : FavouritesScreenEvent
 }
--- feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/presentation/viewmodel/FavouritesState.kt
+++ feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/presentation/viewmodel/FavouritesState.kt
@@ -1,12 +1,12 @@
 package com.epmedu.animeal.favourites.presentation.viewmodel
 
-import com.epmedu.animeal.feeding.domain.model.FeedingPoint
+import com.epmedu.animeal.feeding.presentation.model.FeedingPointModel
 import com.epmedu.animeal.permissions.presentation.PermissionsState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
 data class FavouritesState(
-    val favourites: ImmutableList<FeedingPoint> = persistentListOf(),
-    val showingFeedingPoint: FeedingPoint? = null,
+    val favourites: ImmutableList<FeedingPointModel> = persistentListOf(),
+    val showingFeedingPoint: FeedingPointModel? = null,
     val permissionsState: PermissionsState = PermissionsState()
 )
