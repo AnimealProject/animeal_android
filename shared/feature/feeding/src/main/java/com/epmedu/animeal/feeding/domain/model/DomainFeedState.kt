fa1b692a5db49949400dae168dbfe1ebc73dba69
ozkarram <ozkar.ram.dev@gmail.com>
2023-06-05 09:55:01 -0500

    Refactor DomainFeedState to use domain objects only

--- shared/feature/feeding/src/main/java/com/epmedu/animeal/feeding/domain/model/DomainFeedState.kt
+++ shared/feature/feeding/src/main/java/com/epmedu/animeal/feeding/domain/model/DomainFeedState.kt
@@ -1,9 +1,10 @@
 package com.epmedu.animeal.feeding.domain.model
 
-import com.epmedu.animeal.feeding.presentation.model.FeedingPointModel
-import com.epmedu.animeal.feeding.presentation.viewmodel.FeedingConfirmationState
-
 data class DomainFeedState(
-    val feedPoint: FeedingPointModel?,
-    val feedingConfirmationState: FeedingConfirmationState,
+    val feedPoint: FeedingPoint?,
+    val feedingConfirmationState: DomainFeedingConfirmationState = DomainFeedingConfirmationState.Dismissed,
 )
+
+enum class DomainFeedingConfirmationState {
+    Dismissed, Loading, Showing, FeedingStarted
+}
