b3c5814ea21f42a8fc31a259ae2fc4b3a6143836
Andrei Klepets <ak1988.88@gmail.com>
2023-07-03 11:04:53 -0400

    [EPMEDU-1944] skip 0 minutes in feeding timer

--- library/extensions/src/main/java/com/epmedu/animeal/extensions/Date.kt
+++ library/extensions/src/main/java/com/epmedu/animeal/extensions/Date.kt
@@ -69,8 +69,13 @@ fun tryParseDate(rawDate: String?): LocalDate? {
  */
 fun Context.formatNumberToHourMin(time: Long?): String? {
     if (time == null || time < 0) return null
-    val hours = time / HOUR_IN_MILLIS
-    val minutes = time % HOUR_IN_MILLIS / MINUTE_IN_MILLIS
-
+    var hours = time / HOUR_IN_MILLIS
+    var minutes = time % HOUR_IN_MILLIS / MINUTE_IN_MILLIS
+    if (hours == 0L) {
+        if (minutes == 59L) {
+            hours += 1
+            minutes = 0
+        } else { minutes += 1 }
+    }
     return "${if (hours > 0) "$hours ${getString(R.string.hours)} " else ""}$minutes ${getString(R.string.minutes)}"
 }
