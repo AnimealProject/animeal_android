7c82d2532ac6cb251f9cca466732bc94f954d6ce
yuriiq <8038930+yuriiq@users.noreply.github.com>
2023-08-16 13:04:15 +0200

    [EPMEDU-2503]: Move donate heading to a separate item (#247)

--- feature/tabsflow/moreflow/donate/src/main/java/com/epmedu/animeal/tabs/more/donate/presentation/DonateScreenUI.kt
+++ feature/tabsflow/moreflow/donate/src/main/java/com/epmedu/animeal/tabs/more/donate/presentation/DonateScreenUI.kt
@@ -1,14 +1,21 @@
 package com.epmedu.animeal.tabs.more.donate.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.foundation.layout.LastElementBottom
@@ -26,27 +33,39 @@ internal fun DonateScreenUI(
     state: DonateState,
     onBack: () -> Unit,
 ) {
-    LazyColumn(
-        verticalArrangement = Arrangement.LastElementBottom,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        item {
-            DonateHeading(onBack = onBack)
-            HeightSpacer(height = 43.dp)
-        }
-        items(state.donationInformation) { donateInformation ->
-            DonateListItem(
-                modifier = Modifier.padding(horizontal = 26.dp),
-                donateInformation = donateInformation,
-            )
-            HeightSpacer(height = 32.dp)
-        }
-        item {
-            Text(
-                text = stringResource(id = R.string.donation_thanks),
-                style = MaterialTheme.typography.body1,
-            )
-            HeightSpacer(height = 40.dp)
+    Column {
+        DonateHeading(onBack = onBack)
+        HeightSpacer(height = 43.dp)
+        LazyColumn(
+            verticalArrangement = Arrangement.LastElementBottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            item {
+                Image(
+                    modifier = Modifier
+                        .padding(horizontal = 37.dp)
+                        .clip(RoundedCornerShape(12.dp))
+                        .aspectRatio(2.0f),
+                    painter = painterResource(id = R.drawable.doggo),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+                HeightSpacer(height = 43.dp)
+            }
+            items(state.donationInformation) { donateInformation ->
+                DonateListItem(
+                    modifier = Modifier.padding(horizontal = 26.dp),
+                    donateInformation = donateInformation,
+                )
+                HeightSpacer(height = 32.dp)
+            }
+            item {
+                Text(
+                    text = stringResource(id = R.string.donation_thanks),
+                    style = MaterialTheme.typography.body1,
+                )
+                HeightSpacer(height = 40.dp)
+            }
         }
     }
 }
--- feature/tabsflow/moreflow/donate/src/main/java/com/epmedu/animeal/tabs/more/donate/presentation/ui/DonateHeading.kt
+++ feature/tabsflow/moreflow/donate/src/main/java/com/epmedu/animeal/tabs/more/donate/presentation/ui/DonateHeading.kt
@@ -1,18 +1,12 @@
 package com.epmedu.animeal.tabs.more.donate.presentation.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.foundation.preview.AnimealPreview
@@ -37,16 +31,6 @@ internal fun DonateHeading(onBack: () -> Unit) {
             modifier = Modifier.padding(horizontal = 26.dp),
             style = MaterialTheme.typography.subtitle1,
             text = stringResource(R.string.donation_subtitle),
-        )
-        HeightSpacer(height = 43.dp)
-        Image(
-            modifier = Modifier
-                .padding(horizontal = 37.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .aspectRatio(2.0f),
-            painter = painterResource(id = R.drawable.doggo),
-            contentDescription = null,
-            contentScale = ContentScale.Crop
         )
     }
 }
