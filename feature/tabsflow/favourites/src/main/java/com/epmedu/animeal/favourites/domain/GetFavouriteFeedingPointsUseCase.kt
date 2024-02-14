cb80184b599e2c6a479b4496a30c993190264a44
Vladislav Balatayev <83027107+Fileek@users.noreply.github.com>
2024-02-14 14:08:35 +0100

    [EPMEDU-3089]: Sorting of favorite points list (#273)
    - Add default sorting by feeding point status
    
    Co-authored-by: Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>

--- feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/domain/GetFavouriteFeedingPointsUseCase.kt
+++ feature/tabsflow/favourites/src/main/java/com/epmedu/animeal/favourites/domain/GetFavouriteFeedingPointsUseCase.kt
@@ -1,11 +1,20 @@
 package com.epmedu.animeal.favourites.domain
 
+import com.epmedu.animeal.feeding.domain.model.enum.AnimalState
 import com.epmedu.animeal.feeding.domain.repository.FeedingPointRepository
 import kotlinx.coroutines.flow.map
 
 class GetFavouriteFeedingPointsUseCase(private val feedingPointRepository: FeedingPointRepository) {
 
     operator fun invoke() = feedingPointRepository.getAllFeedingPoints().map { feedingPointsList ->
-        feedingPointsList.filter { feedingPoint -> feedingPoint.isFavourite }
+        feedingPointsList
+            .filter { feedingPoint -> feedingPoint.isFavourite }
+            .sortedBy {
+                when (it.animalStatus) {
+                    AnimalState.RED -> 0
+                    AnimalState.YELLOW -> 1
+                    AnimalState.GREEN -> 2
+                }
+            }
     }
 }
