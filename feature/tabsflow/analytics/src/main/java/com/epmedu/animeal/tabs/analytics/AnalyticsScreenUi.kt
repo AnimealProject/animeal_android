02c9f3026d7c4d0dd9b8db9e3c17148a68e65c62
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2024-03-04 22:38:28 +0100

    Move reusable part to a separate component

--- feature/tabsflow/analytics/src/main/java/com/epmedu/animeal/tabs/analytics/AnalyticsScreenUi.kt
+++ feature/tabsflow/analytics/src/main/java/com/epmedu/animeal/tabs/analytics/AnalyticsScreenUi.kt
@@ -1,22 +1,17 @@
 package com.epmedu.animeal.tabs.analytics
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.epmedu.animeal.foundation.placeholder.ComingSoonStub
 import com.epmedu.animeal.foundation.preview.AnimealPreview
-import com.epmedu.animeal.foundation.spacer.HeightSpacer
 import com.epmedu.animeal.foundation.theme.AnimealTheme
 import com.epmedu.animeal.foundation.topbar.TopBar
 import com.epmedu.animeal.resources.R
@@ -31,36 +26,17 @@ internal fun AnalyticsScreenUi() {
         TopBar(
             title = stringResource(id = R.string.feeding_analysis)
         )
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.weight(0.85f))
-            Box(contentAlignment = Alignment.Center) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_sticker),
-                    contentDescription = null,
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.ic_analysis),
-                    contentDescription = null,
-                    modifier = Modifier.padding(bottom = 12.dp, end = 8.dp)
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.ic_magnifying_glass),
-                    contentDescription = null,
-                    modifier = Modifier.padding(top = 12.dp, start = 8.dp)
-                )
-            }
-            HeightSpacer(height = 16.dp)
-            Text(
-                text = stringResource(id = R.string.coming_soon),
-                color = MaterialTheme.colors.primary,
-                style = MaterialTheme.typography.h1
+        ComingSoonStub {
+            Image(
+                painter = painterResource(id = R.drawable.ic_analysis),
+                contentDescription = null,
+                modifier = Modifier.padding(bottom = 12.dp, end = 8.dp)
             )
-            HeightSpacer(height = 8.dp)
-            Text(text = stringResource(id = R.string.thank_you_for_visiting))
-            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = R.drawable.ic_magnifying_glass),
+                contentDescription = null,
+                modifier = Modifier.padding(top = 12.dp, start = 8.dp)
+            )
         }
     }
 }
