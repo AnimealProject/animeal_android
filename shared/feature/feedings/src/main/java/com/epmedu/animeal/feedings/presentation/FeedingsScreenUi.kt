fa5ac671df22281d6c28f3bd39e14371d61c36d5
ozkarram <ozkar.ram.dev@gmail.com>
2024-03-12 14:16:56 -0500

    Created AnimealPopupScreen for common design screens

--- shared/feature/feedings/src/main/java/com/epmedu/animeal/feedings/presentation/FeedingsScreenUi.kt
+++ shared/feature/feedings/src/main/java/com/epmedu/animeal/feedings/presentation/FeedingsScreenUi.kt
@@ -1,6 +1,5 @@
 package com.epmedu.animeal.feedings.presentation
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,7 +14,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TabRow
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,17 +21,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.epmedu.animeal.feedings.presentation.model.FeedingModel
 import com.epmedu.animeal.feedings.presentation.model.FeedingModelStatus
 import com.epmedu.animeal.feedings.presentation.ui.FeedingItem
 import com.epmedu.animeal.feedings.presentation.viewmodel.FeedingFilterCategory
 import com.epmedu.animeal.feedings.presentation.viewmodel.FeedingsState
+import com.epmedu.animeal.foundation.common.AnimealPopUpScreen
 import com.epmedu.animeal.foundation.preview.AnimealPreview
 import com.epmedu.animeal.foundation.tabs.AnimealSwitchTab
 import com.epmedu.animeal.foundation.tabs.TabIndicator
@@ -126,35 +121,11 @@ private fun EmptyState(isPendingTab: Boolean) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Column(
-            modifier = Modifier.padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-        ) {
-            Image(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(200.dp),
-                contentScale = ContentScale.Fit,
-                painter = painterResource(R.drawable.empty_screen_bone),
-                contentDescription = null
-            )
-            Text(
-                text = stringResource(
-                    id = if (isPendingTab) R.string.feeding_tab_all_reviewed_title else R.string.feeding_tab_empty_title
-                ),
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.h5,
-                textAlign = TextAlign.Center,
-            )
-            Text(
-                text = stringResource(
-                    id = if (isPendingTab) R.string.feeding_tab_all_reviewed_subtitle else R.string.feeding_tab_empty_subtitle
-                ),
-                style = MaterialTheme.typography.body1,
-                textAlign = TextAlign.Center,
-            )
-        }
+        AnimealPopUpScreen(
+            painterResource = painterResource(R.drawable.empty_screen_bone),
+            titleText = if (isPendingTab) R.string.feeding_tab_all_reviewed_title else R.string.feeding_tab_empty_title,
+            subtitleText = if (isPendingTab) R.string.feeding_tab_all_reviewed_subtitle else R.string.feeding_tab_empty_subtitle
+        )
     }
 }
 
