af8e31dfe6cbdbd8c812699807876d10a5e7f421
Vladislav Balatayev <83027107+Fileek@users.noreply.github.com>
2024-04-10 22:56:02 +0200

    Refactor contentAlphaButtonAlpha function (#296)
    Co-authored-by: Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>

--- library/foundation/src/main/java/com/epmedu/animeal/foundation/bottombar/BottomBarVisibility.kt
+++ library/foundation/src/main/java/com/epmedu/animeal/foundation/bottombar/BottomBarVisibility.kt
@@ -1,8 +1,10 @@
 package com.epmedu.animeal.foundation.bottombar
 
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.staticCompositionLocalOf
+import com.epmedu.animeal.foundation.bottomsheet.AnimealBottomSheetState
 
 val LocalBottomBarVisibilityController =
     staticCompositionLocalOf<(BottomBarVisibilityState) -> Unit> { {} }
@@ -13,5 +15,19 @@ fun BottomBarVisibility(state: BottomBarVisibilityState) {
 
     LaunchedEffect(Unit) {
         bottomBarVisibilityController(state)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BottomBarVisibility(state: AnimealBottomSheetState) {
+    val bottomBarVisibilityController = LocalBottomBarVisibilityController.current
+
+    with(state) {
+        LaunchedEffect(progress, currentValue) {
+            val showBottomBar = isShowing.not() && isExpanding.not() && isHidden
+
+            bottomBarVisibilityController(BottomBarVisibilityState.ofBoolean(showBottomBar))
+        }
     }
 }
