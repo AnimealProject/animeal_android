700600781be4c512bc5dfa6105bc16eb193bf028
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2024-05-12 03:07:34 +0200

    Add shimmer loading for photos on feeding card

--- shared/feature/feedings/src/main/java/com/epmedu/animeal/feedings/presentation/ui/FeedingPhotoCarousel.kt
+++ shared/feature/feedings/src/main/java/com/epmedu/animeal/feedings/presentation/ui/FeedingPhotoCarousel.kt
@@ -2,11 +2,17 @@ package com.epmedu.animeal.feedings.presentation.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -14,8 +20,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter.State.Loading
 import coil.request.ImageRequest
 import com.epmedu.animeal.feedings.presentation.model.FeedingModel
+import com.epmedu.animeal.foundation.loading.ShimmerLoading
 import com.epmedu.animeal.foundation.spacer.WidthSpacer
 
 @Composable
@@ -29,20 +37,35 @@ internal fun FeedingPhotoCarousel(
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         itemsIndexed(feeding.photos) { index, photo ->
+            var isPhotoLoading by rememberSaveable(feeding.id) { mutableStateOf(false) }
+
             if (index == 0) WidthSpacer(width = 30.dp)
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(photo.url)
-                    .diskCacheKey(photo.name)
-                    .crossfade(true)
-                    .build(),
-                contentScale = ContentScale.Crop,
-                contentDescription = null,
+
+            Box(
                 modifier = Modifier
                     .size(66.dp)
                     .clip(RoundedCornerShape(8.dp))
                     .clickable { onPhotoClick(index) }
-            )
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(photo.url)
+                        .diskCacheKey(photo.name)
+                        .crossfade(true)
+                        .build(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                    onState = { state ->
+                        isPhotoLoading = state is Loading
+                    }
+                )
+                if (isPhotoLoading) {
+                    ShimmerLoading(
+                        modifier = Modifier.fillMaxSize(),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                }
+            }
             if (index == feeding.photos.lastIndex) WidthSpacer(width = 30.dp)
         }
     }
