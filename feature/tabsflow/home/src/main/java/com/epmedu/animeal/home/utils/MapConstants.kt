efcc353962ce98ed04eb3b2fe33c05a6287687cb
Vladislav Balatayev <83027107+Fileek@users.noreply.github.com>
2024-05-27 12:19:47 +0200

    [EPMEDU-3284]: Non intuitive behavior of app (#310)
    - Reduce map zoom to show a whole city
    
    Co-authored-by: Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>

--- feature/tabsflow/home/src/main/java/com/epmedu/animeal/home/utils/MapConstants.kt
+++ feature/tabsflow/home/src/main/java/com/epmedu/animeal/home/utils/MapConstants.kt
@@ -1,7 +1,7 @@
 package com.epmedu.animeal.home.utils
 
 object MapConstants {
-    const val DEFAULT_ZOOM = 13.0
+    const val DEFAULT_ZOOM = 10.0
     const val DEFAULT_MAP_START_PADDING = 200.0
     const val DEFAULT_MAP_END_PADDING = 200.0
     const val DEFAULT_MAP_TOP_PADDING = 300.0
