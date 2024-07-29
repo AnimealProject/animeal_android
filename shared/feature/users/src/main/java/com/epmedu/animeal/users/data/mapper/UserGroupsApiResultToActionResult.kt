d77a42aa6f1aeccc9684e8988feef70b5cc42823
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2024-07-30 01:03:21 +0200

    Fix detekt

--- shared/feature/users/src/main/java/com/epmedu/animeal/users/data/mapper/UserGroupsApiResultToActionResult.kt
+++ shared/feature/users/src/main/java/com/epmedu/animeal/users/data/mapper/UserGroupsApiResultToActionResult.kt
@@ -2,9 +2,9 @@ package com.epmedu.animeal.users.data.mapper
 
 import com.epmedu.animeal.common.data.wrapper.ApiResult
 import com.epmedu.animeal.common.domain.wrapper.ActionResult
+import com.epmedu.animeal.users.domain.model.UserGroup
 import com.epmedu.animeal.users.data.model.UserGroups as DataUserGroups
 import com.epmedu.animeal.users.domain.model.UserGroup as DomainUserGroup
-import com.epmedu.animeal.users.domain.model.UserGroup
 
 internal fun ApiResult<DataUserGroups>.toActionResult(): ActionResult<List<DomainUserGroup>> {
     return when (this) {
