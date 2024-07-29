d63f66d4a59598564a7d5c164f616c2de00907cf
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2024-07-30 00:15:20 +0200

    Rename constant which contain lowercase letters

--- library/auth/src/main/java/com/epmedu/animeal/auth/constants/UserAttributesKey.kt
+++ library/auth/src/main/java/com/epmedu/animeal/auth/constants/UserAttributesKey.kt
@@ -3,7 +3,7 @@ package com.epmedu.animeal.auth.constants
 import com.amplifyframework.auth.AuthUserAttributeKey
 
 object UserAttributesKey {
-    const val phoneNumberVerifiedKey = "phone_number_verified"
+    const val PHONE_NUMBER_VERIFIED_KEY = "phone_number_verified"
 
     val nameKey: AuthUserAttributeKey = AuthUserAttributeKey.name()
     val surnameKey: AuthUserAttributeKey = AuthUserAttributeKey.familyName()
