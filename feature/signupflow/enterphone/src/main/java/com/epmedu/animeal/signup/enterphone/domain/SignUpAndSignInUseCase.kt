a988827478a56845ffd73c54eec0648281698814
Vladislav Balatayev <83027107+Fileek@users.noreply.github.com>
2024-09-03 13:40:14 +0200

    [EPMEDU-3812]: Add new error exception during sign in (#348)
    - Handle UserNotFoundException on sign in with changed phone number that is not registered yet
    
    Co-authored-by: Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>

--- feature/signupflow/enterphone/src/main/java/com/epmedu/animeal/signup/enterphone/domain/SignUpAndSignInUseCase.kt
+++ feature/signupflow/enterphone/src/main/java/com/epmedu/animeal/signup/enterphone/domain/SignUpAndSignInUseCase.kt
@@ -6,6 +6,7 @@ import com.amplifyframework.auth.exceptions.InvalidStateException
 import com.epmedu.animeal.common.domain.wrapper.ActionResult
 
 class SignUpAndSignInUseCase(private val repository: EnterPhoneRepository) {
+
     suspend operator fun invoke(
         phone: String,
         password: String,
@@ -17,22 +18,39 @@ class SignUpAndSignInUseCase(private val repository: EnterPhoneRepository) {
 
             is ActionResult.Failure -> {
                 when (result.error) {
-                    is UsernameExistsException,
-                    is InvalidStateException,
-                    is UserNotFoundException -> {
+                    is UsernameExistsException -> {
                         signIn(phone)
                     }
+                    // this is thrown when the user is already signed in
+                    is InvalidStateException -> {
+                        handleInvalidStateException(phone, password)
+                    }
                     else -> {
-                        ActionResult.Failure(result.error)
+                        result
                     }
                 }
             }
         }
     }
 
-    private suspend fun signIn(
-        phone: String,
-    ): ActionResult<Unit> {
+    private suspend fun signIn(phone: String): ActionResult<Unit> {
         return repository.signIn(phone)
+    }
+
+    private suspend fun handleInvalidStateException(
+        phone: String,
+        password: String
+    ): ActionResult<Unit> {
+        // We can't sign out the user here because the OTP is not confirmed yet.
+        // So the only way to handle this is to try to sign in the user.
+        val result = signIn(phone)
+        return when {
+            result is ActionResult.Failure && result.error is UserNotFoundException -> {
+                invoke(phone, password)
+            }
+            else -> {
+                result
+            }
+        }
     }
 }
