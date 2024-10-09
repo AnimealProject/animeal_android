605156f37a25ce6785984314076f720f1f51be8c
Uladzislau Balatayeu <uladzislau_balatayeu@epam.com>
2024-10-09 16:05:07 +0200

    Bump Android Gradle Plugin version to 8.7.0

--- buildSrc/src/main/java/com/epmedu/animeal/configure/AppPluginSection.kt
+++ buildSrc/src/main/java/com/epmedu/animeal/configure/AppPluginSection.kt
@@ -40,8 +40,8 @@ internal fun Project.configureAndroidApplication() = applicationExtension.run {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     flavorDimensions += "environment"
