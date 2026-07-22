636b1ee36fffbf8ff106a0b705626dc241526e37
AliaksandrShustouski <aliaksandr_shustouski@epam.com>
2026-07-22 08:36:13 +0300

    fix: changed zoom show on map to 20.0 (#413)

--- feature/tabsflow/home/src/main/java/com/epmedu/animeal/home/utils/MapConstants.kt
+++ feature/tabsflow/home/src/main/java/com/epmedu/animeal/home/utils/MapConstants.kt
@@ -2,7 +2,7 @@ package com.epmedu.animeal.home.utils
 
 object MapConstants {
     const val DEFAULT_ZOOM = 10.0
-    const val DEFAULT_SHOW_ON_MAP_ZOOM = 17.0
+    const val DEFAULT_SHOW_ON_MAP_ZOOM = 20.0
     const val DEFAULT_MAP_START_PADDING = 200.0
     const val DEFAULT_MAP_END_PADDING = 200.0
     const val DEFAULT_MAP_TOP_PADDING = 300.0
